3f8f80ee77b0448bd3081be7b2f10eef0c4b370b
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2026-05-21 22:53:30 +0530

    currying

---
+++ currying.js
@@ -0,0 +1,24 @@
+//decompose the function to take single/ double args accordign to use
+
+//using bind
+let multiply= function(x,y){
+    console.log(x*y);
+}
+
+//now using bind we only use the required parameter and pass in an arg to the parameter fn
+let multiplybytwo= multiply(this, 2);
+multiplybytwo(3); //gives 6
+
+let multiplybythree= multiply.bind(this,3);
+multiplybythree(5);
+
+
+//using closures
+let multiply= function(x){
+    let innerfn=function(y){
+        console.log(x*y);
+    }
+}
+
+let multiplybytwo = multiply(2);
+multiplybytwo(3);
