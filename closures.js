3214a0ac80d3d2b3a8eb714634e3301c2c63dd56
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2026-05-29 23:53:12 +0530

    callbacks

--- closures.js
+++ closures.js
@@ -14,3 +14,35 @@ console.log(counter()); // 1
 console.log(counter()); // 2
 console.log(counter2()); // 1 since in others a reference is created but in case of coutner 2 count is old and no link
 console.log(counter()); // 3
+
+
+//find the output
+let name = "Global";
+
+function printName() {
+  console.log(name);
+}
+
+function test() {
+  let name = "Local";
+
+  printName();
+}
+
+test();
+//Global-- since the function remember the scope of its creation not calling (lexical)
+
+
+//find output
+function createLogger(prefix) {
+
+  return function(message) {
+    console.log(`${prefix}: ${message}`);
+  }
+
+}
+
+const errorLogger = createLogger("ERROR");
+
+errorLogger("DB Down");
+//ERROR: DB Down
