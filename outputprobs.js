227bc867c4e29194bee45074dc1eb9e15296aba7
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2026-05-30 23:49:23 +0530

    output predict

---
+++ outputprobs.js
@@ -0,0 +1,31 @@
+console.log([] == ![]);
+console.log([] === ![]);
+console.log([0] == false);
+console.log([0] === false);
+console.log(null == 0);
+console.log(undefined == 0);
+console.log("" == false);
+console.log("0" == false);
+console.log([] == "");
+console.log([1] == true);
+
+//learning
+[] == false          // true
+[] == 0              // true
+[] == ""             // true
+
+null == undefined    // true
+
+NaN == NaN           // false
+
+{} == {}             // false
+[] == []             // false
+
+"5" == 5             // true
+"5" === 5            // false
+
+false == 0           // true
+false === 0          // false
+
+Boolean([])          // true
+Boolean({})          // true
